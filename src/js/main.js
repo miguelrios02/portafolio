7f37af90ef7692ea1986ff27755d56f03c7b5023
mianriga12 <mianriga@gmail.com>
2022-11-05 15:06:12 -0500

    first commit

---
+++ src/js/main.js
@@ -0,0 +1,6 @@
+const iconMenu = document.querySelector(".bx-menu");
+const menu = document.querySelector(".menu");
+
+iconMenu.addEventListener("click", function () {
+    console.log(menu.classList.toggle("menu-show"));
+});
