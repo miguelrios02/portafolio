3d6029b21d86a0f3db11ec0e7d484910b7f3248d
mianriga12 <mianriga@gmail.com>
2023-01-12 15:10:42 -0500

    styles

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,23 @@
 const iconMenu = document.querySelector(".bx-menu");
 const menu = document.querySelector(".menu");
+const bxMenu = document.querySelector("menu__button");
 
 iconMenu.addEventListener("click", function () {
-    console.log(menu.classList.toggle("menu-show"));
+    menu.classList.toggle("menu-show");
+    
 });
 
+menu.addEventListener("click",(e)=> {
 
+    if(e.target.classList.contains("menu__button")){
+        menu.classList.toggle("menu-show");
+    }
+});
+
+window.addEventListener("load",function (){
+    const loading = this.document.querySelector(".loading");
+    
+    setTimeout(()=>{
+        loading.style.display="none";
+    },500);
+})
