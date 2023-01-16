a78691371dee1269ab3dc5051c01ec27bae3e5e1
mianriga12 <mianriga@gmail.com>
2023-01-16 10:35:49 -0500

    form

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,9 @@
 const iconMenu = document.querySelector(".bxmenu");
 const menu = document.querySelector(".menu");
 const bxMenu = document.querySelector("menu__button");
+const formRegister = document.querySelector("#formRegister");
+const formContainer = document.querySelector(".formContainer");
+
 
 iconMenu.addEventListener("click", function () {
     menu.classList.toggle("menu-show");
@@ -24,4 +27,7 @@ window.addEventListener("load",function (){
     setTimeout(()=>{
         loading.style.display="none";
     },500);
+    
 })
+
+
